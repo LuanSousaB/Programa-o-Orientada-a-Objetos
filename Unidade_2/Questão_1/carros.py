7c0803fd6e5393939561ebbc3adfb55d611bb56d
LuanSousaB <luan.sousab2004@gmail.com>
2025-09-14 22:53:31 -0300

    Add files via upload

--- Unidade_2/Questão_1/carros.py
+++ Unidade_2/Questão_1/carros.py
@@ -1,1 +1,21 @@
+class Carro:
+    def __init__(self, marca, modelo, ano, cor):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.cor = cor
 
+carro1 = Carro("Chevrolet", "Chevette", 1985, "Vermelho")
+carro2 = Carro("Chevrolet", "Opala", 1990, "Azul")
+
+print("Carro 1:")
+print("Marca:", carro1.marca)
+print("Modelo:", carro1.modelo)
+print("Ano:", carro1.ano)
+print("Cor:", carro1.cor)
+
+print("\nCarro 2:")
+print("Marca:", carro2.marca)
+print("Modelo:", carro2.modelo)
+print("Ano:", carro2.ano)
+print("Cor:", carro2.cor)
